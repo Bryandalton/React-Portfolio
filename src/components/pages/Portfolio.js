e594511253bfe02bd398d732e500bdae3a91cd36
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2023-01-23 22:00:08 -0500

    relative file paths

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,25 +3,25 @@ import Project from '../Project';
 
 const projects  = [
   {
-    img: '/images/Paint Your Mood.png',
+    img: './images/Paint Your Mood.png',
     name: 'Paint your mood',
     link: 'https://bryandalton.github.io/Paint-your-mood/',
     github: 'https://github.com/Bryandalton/Paint-your-mood'
   },
   {
-    img: '/images/Ripened-potatos-screenshot.jpg',
+    img: './images/Ripened-potatos-screenshot.jpg',
     name: 'Ripened Potatoes',
     link: 'https://ripened-potatos.herokuapp.com/',
     github: 'https://github.com/Bryandalton/Ripened_Potatoes'
   },
   {
-    img: '/images/WeatherServiceScreenshot.jpg',
+    img: './images/WeatherServiceScreenshot.jpg',
     name: 'Weather Service',
     link: 'https://bryandalton.github.io/Weather-service/',
     github: 'https://github.com/Bryandalton/Weather-service'
   },
   {
-    img: '/images/Bigfootbutton.jpg',
+    img: './images/Bigfootbutton.jpg',
     name: 'Bigfoot Button',
     link: 'https://codepen.io/Iwritebadcode/full/mdXdxpz',
     github: 'https://codepen.io/Iwritebadcode/pen/mdXdxpz'
